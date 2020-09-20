b194c36ee975500a47212a2ab169a27455794c26
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-09-20 22:10:14 +0200

    Added some comments. (#4)

--- src/transformers/youtube.ts
+++ src/transformers/youtube.ts
@@ -1,19 +1,23 @@
 /**
- * If the URL is a youtube our, then makes sure that the URL is always formatted
- * the same way.
+ * If the URL is a youtube video, then makes sure that the URL is always
+ * formatted the same way.
  */
 export const transformYoutube = (url: string): string => {
+  // Skip all non-youtube related domains.
   if (!url.includes("youtu.be/") && !url.includes("youtube.com/watch?")) {
     return url;
   }
   const obj = new URL(url);
   const params = obj.searchParams;
+  // Convert youtu.be -> youtube.com, move the videoId from the path to a GET
+  // parameter.
   if (obj.host === "youtu.be") {
     obj.host = "www.youtube.com";
     const videoId = obj.pathname.slice(1);
     obj.pathname = "/watch";
     params.set("v", videoId);
   }
+  // Implicitly, remove any GET parameters not being "v".
   const newParams = new URLSearchParams();
   if (params.has("v")) {
     newParams.set("v", params.get("v")!);
