4e04725fc0a5ef4703a80c2c7bc168abaf5e2fcb
Dennis Hedegaard <dennis@dhedegaard.dk>
2021-05-02 17:43:56 +0200

    Attempted fix at permission check issue

--- src/deps.ts
+++ src/deps.ts
@@ -2,3 +2,5 @@ export { formatDistance } from "https://cdn.skypack.dev/date-fns@2.21.1";
 export { ensureDir } from "https://deno.land/std@0.95.0/fs/ensure_dir.ts";
 export { dirname, join } from "https://deno.land/std@0.95.0/path/mod.ts";
 export * from "https://deno.land/x/discordeno@10.5.0/mod.ts";
+export { RequestManager } from "https://deno.land/x/discordeno@10.5.0/src/rest/request_manager";
+export { endpoints } from "https://deno.land/x/discordeno@10.5.0/src/util/constants";
