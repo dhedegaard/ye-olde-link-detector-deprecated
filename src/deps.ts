8cedb709aa618d1039ca95e62da486c5d780f524
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-06-29 20:55:03 +0200

    bumped std deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,5 @@
 export { formatDistance } from "https://cdn.skypack.dev/date-fns@2.21.1";
-export { ensureDir } from "https://deno.land/std@0.95.0/fs/ensure_dir.ts";
-export { dirname, join } from "https://deno.land/std@0.95.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.145.0/fs/ensure_dir.ts";
+export { dirname, join } from "https://deno.land/std@0.145.0/path/mod.ts";
 export * as discord from "https://deno.land/x/discordeno@12.0.1/mod.ts";
-// export { RequestManager } from "https://deno.land/x/discordeno@12.0.1/src/rest/request_manager.ts";
 export { endpoints } from "https://deno.land/x/discordeno@12.0.1/src/util/constants.ts";
